8493418c85d8edfff7acc6f6a9e0c720d8e6b938
kittykittycat <naitsabes1248@gmail.com>
2026-01-29 19:21:22 +0100

    added length tests

--- spi/test.py
+++ spi/test.py
@@ -4,6 +4,8 @@ from message import Message, Message_Type
 
 class TestSerialization(unittest.TestCase):
 
+    # Equality of Message and Serialize(Deserialize(Message))
+
     def test_open(self):
         p = Message(Message_Type.REQUEST_OPEN)
         self.assertEqual(p, Message.deserialize(p.serialize()))
@@ -27,6 +29,16 @@ class TestSerialization(unittest.TestCase):
     def test_error(self):
         p = Message(Message_Type.ERROR)
         self.assertEqual(p, Message.deserialize(p.serialize()))
+
+    # Length
+
+    def test_length_0bytes(self):
+        p = Message(Message_Type.REQUEST_OPEN)
+        self.assertEqual(len(p.content), 0)
+
+    def test_length_32byte(self):
+        p = Message(Message_Type.CHALLENGE)
+        self.assertEqual(len(p.content), 32)
 
 if __name__ == "__main__":
     unittest.main()
