f1915b367c550169648f463b47bdc37a61505d28
kittykittycat <naitsabes1248@gmail.com>
2026-01-29 10:28:42 +0100

    removed useless queue in slave

--- spi/slave.py
+++ spi/slave.py
@@ -1,5 +1,4 @@
 #Lock
-import queue
 import secrets
 
 from cryptomodule import Packet_Type, Packet, challenge_result
@@ -13,28 +12,24 @@ def spi_slave():
     response : Packet
     expected_result : bytes = rng()
     while True:
-        try:
-            request = com.receive()
+        request = com.receive()
 
-            if request.type == Packet_Type.REQUEST_OPEN:
-                random = rng()
-                response = Packet(Packet_Type.CHALLENGE, random)
-                expected_result = challenge_result(random)
-            elif request.type == Packet_Type.CHALLENGE_ANSWER:
-                if expected_result == request.content:
-                    open()
-                    response = Packet(Packet_Type.GRANT_ACCESS)
-                    expected_result = rng()
-                else:
-                    deny()
-                    response = Packet(Packet_Type.DENY_ACCESS)
+        if request.type == Packet_Type.REQUEST_OPEN:
+            random = rng()
+            response = Packet(Packet_Type.CHALLENGE, random)
+            expected_result = challenge_result(random)
+        elif request.type == Packet_Type.CHALLENGE_ANSWER:
+            if expected_result == request.content:
+                open()
+                response = Packet(Packet_Type.GRANT_ACCESS)
+                expected_result = rng()
             else:
-                response = Packet(Packet_Type.ERROR)
+                deny()
+                response = Packet(Packet_Type.DENY_ACCESS)
+        else:
+            response = Packet(Packet_Type.ERROR)
 
-            com.send(response)
-
-        except queue.Empty:
-            continue
+        com.send(response)
 
 def rng() -> bytes:
     return secrets.token_bytes(RANDOM_BYTES_COUNT)
