62f787ca61de3336693536e157b60fedeb9082af
kittykittycat <naitsabes1248@gmail.com>
2026-01-30 14:57:34 +0100

    added custom ascon

--- challenge_response_python/cryptomodule.py
+++ challenge_response_python/cryptomodule.py
@@ -1,3 +1,4 @@
+import custom_ascon.custom_ascon as custom_ascon
 import ascon
 from Crypto.Hash import KMAC128
 
@@ -6,6 +7,10 @@ from message import BYTE_ORDER
 SECRET_KEY : int = 0b1010101010101010101010101010101010101010101010101010101010101010
 SECRET_KEY_BYTES : bytes = SECRET_KEY.to_bytes(16, byteorder=BYTE_ORDER)
 
+
+def custom_asconHash256(message : bytes) -> bytes:
+    return custom_ascon.ascon_hash(message)
+
 def asconHash256(message : bytes) -> bytes:
     return ascon.hash(message)
 
@@ -13,9 +18,12 @@ def kmac128(key : bytes, message : bytes) -> bytes:
     kmac = KMAC128.new(key=key, data=message, mac_len=32)
     return kmac.digest()
 
-def digest(nonce : bytes) -> bytes:
-    return asconHash256(kmac128(SECRET_KEY_BYTES, nonce))
+def digest(nonce : bytes, custom : bool = False) -> bytes:
+    return (
+        custom_asconHash256(kmac128(SECRET_KEY_BYTES, nonce)) if custom else asconHash256(kmac128(SECRET_KEY_BYTES, nonce))
+    )
 
 if __name__ == "__main__":
+    print("Custom Ascon Hash of 'Hello, World!':", custom_asconHash256(message=b"Hello, World!").hex())
     print("Ascon Hash of 'Hello, World!':", asconHash256(message=b"Hello, World!").hex())
     print("KMAC of 'Hello, World!':", digest(nonce=b"Hello, World!").hex())
