ddbb08b3cb5f0a380edc871b5fded1bf15120847
Anna-Livia Martin <119300861+AnnaLiviaMartin@users.noreply.github.com>
2026-02-21 11:43:17 +0100

    try: going back to basics

--- spi-systemverilog/spi.sv
+++ spi-systemverilog/spi.sv
@@ -63,20 +63,18 @@ always @(posedge clk)
 if(SSEL_active)
 begin
   if(SSEL_startmessage)
-    byte_data_sent <= 8'h00;  // first byte sent in a message is the message count
+    byte_data_sent <= byte_data_received;  // first byte sent in a message is the message count
   else
   if(SCK_fallingedge)
+  begin
+    if(bitcnt==3'b000)
       byte_data_sent <= 8'h00;  // after that, we send 0s
-  else
-  if(valid_out)
-      MISO <= byte_data_sent;
-  //else begin
-  //      byte_data_sent <= byte_data_received;
-  //  end
-      //byte_data_sent <= {byte_data_sent[6:0], 1'b0}; // 8'h05;
+    else
+      byte_data_sent <= {byte_data_sent[6:0], 1'b0};
+  end
 end
 
-//assign MISO = byte_data_sent[7];  // send MSB first
+assign MISO = byte_data_sent[7];  // send MSB first
 // we assume that there is only one slave on the SPI bus
 // so we don't bother with a tri-state buffer for MISO
 // otherwise we would need to tri-state MISO when SSEL is inactive
